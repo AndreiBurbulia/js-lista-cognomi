2b094e6d11f7fa38bc208c9676e4fafaa211ffa2
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-28 15:35:24 +0200

    Struutura funzionante

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
 
 
 //chiedere all'utente in cognome 
-
+var cognomeInserito = prompt("Inserisci il tuo cognome!!");
+var cognomeUtente = cognomeInserito.toLowerCase();
+console.log(cognomeUtente);
 //inserire il cognome dell'utente in un array contenente altri cognomi 
+
+var listaCognomi = ["rossi", "pinco", "ponchia", "buttafoco", "moro", "zardin", "aldi"];
+listaCognomi.push(cognomeUtente);
+console.log(listaCognomi);
 
 //stampa la lista ordinata alfabeticamente dei cognomi presenti nel array
 
+var listaOrdinata = listaCognomi.sort();
+console.log(listaOrdinata);
+
+for (var i = 0; i < listaOrdinata.length; i++) {
+    console.log((i + 1) + " " + listaOrdinata[i]);
+}
+
 //scriver eanche la posizione in cui si trova il cognome inserito dall'utente partendo dalla posizione 1
+
+var posizioneCognome = listaOrdinata.indexOf(cognomeUtente);
+console.log(posizioneCognome + 1);
