2772f06cf3e7ce5c51fc71a4f3bca67d1fa541f3
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-28 15:44:44 +0200

    esercizio completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,23 @@
 //chiedere all'utente in cognome 
 var cognomeInserito = prompt("Inserisci il tuo cognome!!");
 var cognomeUtente = cognomeInserito.toLowerCase();
-console.log(cognomeUtente);
+
 //inserire il cognome dell'utente in un array contenente altri cognomi 
 
 var listaCognomi = ["rossi", "pinco", "ponchia", "buttafoco", "moro", "zardin", "aldi"];
 listaCognomi.push(cognomeUtente);
-console.log(listaCognomi);
 
 //stampa la lista ordinata alfabeticamente dei cognomi presenti nel array
 
 var listaOrdinata = listaCognomi.sort();
-console.log(listaOrdinata);
 
 for (var i = 0; i < listaOrdinata.length; i++) {
-    console.log((i + 1) + " " + listaOrdinata[i]);
+    var posizione = listaOrdinata[i];
+    contenutoLista = document.getElementById("lista").innerHTML;
+    document.getElementById("lista").innerHTML = contenutoLista + "<li>" + posizione + "</li>";
 }
 
 //scriver eanche la posizione in cui si trova il cognome inserito dall'utente partendo dalla posizione 1
 
 var posizioneCognome = listaOrdinata.indexOf(cognomeUtente);
-console.log(posizioneCognome + 1);
+document.getElementById("posizione_cognome").innerHTML = posizioneCognome + 1;
