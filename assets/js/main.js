721d4741d2822ee2b8ebe419038a38024cf3f5af
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-28 15:57:36 +0200

    Pulizia Codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,10 @@ var cognomeInserito = prompt("Inserisci il tuo cognome!!");
 var cognomeUtente = cognomeInserito.toLowerCase();
 
 //inserire il cognome dell'utente in un array contenente altri cognomi 
-
 var listaCognomi = ["rossi", "pinco", "ponchia", "buttafoco", "moro", "zardin", "aldi"];
 listaCognomi.push(cognomeUtente);
 
 //stampa la lista ordinata alfabeticamente dei cognomi presenti nel array
-
 var listaOrdinata = listaCognomi.sort();
 
 for (var i = 0; i < listaOrdinata.length; i++) {
@@ -19,7 +17,7 @@ for (var i = 0; i < listaOrdinata.length; i++) {
     document.getElementById("lista").innerHTML = contenutoLista + "<li>" + posizione + "</li>";
 }
 
-//scriver eanche la posizione in cui si trova il cognome inserito dall'utente partendo dalla posizione 1
 
+//scriver eanche la posizione in cui si trova il cognome inserito dall'utente partendo dalla posizione 1
 var posizioneCognome = listaOrdinata.indexOf(cognomeUtente);
 document.getElementById("posizione_cognome").innerHTML = posizioneCognome + 1;
