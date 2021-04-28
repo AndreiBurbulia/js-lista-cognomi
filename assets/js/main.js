9d81d8f2c6a6c1379d6d65cbc74b5e5c5a3cdca1
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-28 15:18:38 +0200

    File

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+//chiedere all'utente in cognome 
+
+//inserire il cognome dell'utente in un array contenente altri cognomi 
+
+//stampa la lista ordinata alfabeticamente dei cognomi presenti nel array
+
+//scriver eanche la posizione in cui si trova il cognome inserito dall'utente partendo dalla posizione 1
